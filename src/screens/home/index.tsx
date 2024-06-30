eda7a0d3dc838913faf22a09e3c6984a1b8f388a
Pedro Samuel <pedro.samuel@onesolutions.com.br>
2024-06-30 20:23:00 -0300

    refactor: Remove unused new task input and button from Home screen

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -2,12 +2,14 @@ import { View, Text, TextInput, TouchableOpacity, Image } from "react-native";
 import { styles } from "./styles";
 import { useFonts } from "expo-font";
 import { Inter_400Regular, Inter_700Bold } from "@expo-google-fonts/inter";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Header from "../../components/Header";
 import Tasks from "../../components/Tasks";
 
 export default function Home() {
   const [fontsLoaded] = useFonts({ Inter_400Regular, Inter_700Bold });
+  const [newTask, setNewTask] = useState("");
+  const [newTaskFocused, setNewTaskFocused] = useState(false);
   const [isPressed, setIsPressed] = useState(false);
 
   if (!fontsLoaded) {
@@ -19,22 +21,6 @@ export default function Home() {
       <View style={styles.container}>
         <Header />
         <View style={styles.tasksContainer}>
-          <View style={styles.newTask}>
-            <TextInput
-              style={styles.inputText}
-              placeholder={"Adicione uma nova tarefa"}
-              placeholderTextColor={"#808080"}
-            />
-            <TouchableOpacity
-              style={[styles.button, isPressed && styles.buttonHover]}
-              onPressIn={() => setIsPressed(true)}
-              onPressOut={() => setIsPressed(false)}
-              activeOpacity={0.8}
-              onPress={() => console.log("Button pressed")}
-            >
-              <Text style={styles.buttonText}>+</Text>
-            </TouchableOpacity>
-          </View>
           <Tasks />
         </View>
       </View>
--- src/screens/home/styles.ts
+++ src/screens/home/styles.ts
@@ -10,46 +10,6 @@ export const styles = StyleSheet.create({
     flex: 1,
     width: "100%",
     padding: 24,
-    top: -52,
-  },
-
-  newTask: {
-    width: "100%",
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
-    gap: 8,
-    marginBottom: 32,
-  },
-
-  inputText: {
-    flex: 1,
-    height: 52,
-    paddingHorizontal: 16,
-    borderRadius: 6,
-    fontSize: 16,
-    backgroundColor: "#262626",
-    color: "#F2F2F2",
-    borderWidth: 1,
-    borderColor: "#0D0D0D",
-    fontFamily: "Inter_400Regular",
-  },
-
-  button: {
-    width: 52,
-    height: 52,
-    backgroundColor: "#1E6F9F",
-    borderRadius: 6,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-
-  buttonHover: {
-    backgroundColor: "#4EA8DE",
-  },
-
-  buttonText: {
-    color: "#F2F2F2",
-    fontSize: 24,
+    paddingTop: 50,
   },
 });
