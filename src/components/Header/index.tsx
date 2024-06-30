25f0e8815b230ab7ed4dfb50e0b7fa72c1f7d2eb
Pedro Samuel <pedro.samuel@onesolutions.com.br>
2024-06-30 20:22:37 -0300

    feat: Add new task input and button to Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,34 @@
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, TextInput, TouchableOpacity } from "react-native";
 import { styles } from "./styles";
+import { useState } from "react";
 
 export default function Header() {
+  const [newTask, setNewTask] = useState("");
+  const [newTaskFocused, setNewTaskFocused] = useState(false);
+  const [isPressed, setIsPressed] = useState(false);
+
   return (
     <View style={styles.background}>
       <Image source={require("../../assets/Logo.png")} />
+      <View style={styles.newTask}>
+        <TextInput
+          style={[styles.inputText, newTaskFocused && styles.inputTextFocused]}
+          placeholder={"Adicione uma nova tarefa"}
+          placeholderTextColor={"#808080"}
+          onChange={() => setNewTask}
+          onFocus={() => setNewTaskFocused(true)}
+          onBlur={() => setNewTaskFocused(false)}
+        />
+        <TouchableOpacity
+          style={[styles.button, isPressed && styles.buttonHover]}
+          onPressIn={() => setIsPressed(true)}
+          onPressOut={() => setIsPressed(false)}
+          activeOpacity={0.8}
+          onPress={() => console.log("Button pressed")}
+        >
+          <Text style={styles.buttonText}>+</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -6,13 +6,59 @@ const colors = {
 
 export const styles = StyleSheet.create({
   background: {
+    position: "relative",
     height: 173,
     width: "100%",
     backgroundColor: colors.background,
     padding: 24,
-    flexDirection: "row",
+    flexDirection: "column",
     justifyContent: "center",
     alignItems: "center",
     gap: 8,
+  },
+
+  newTask: {
+    position: "absolute",
+    width: "100%",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    gap: 8,
+    bottom: -22,
+  },
+
+  inputText: {
+    flex: 1,
+    height: 52,
+    paddingHorizontal: 16,
+    borderRadius: 6,
+    fontSize: 16,
+    backgroundColor: "#262626",
+    color: "#F2F2F2",
+    borderWidth: 1,
+    borderColor: "#0D0D0D",
+    fontFamily: "Inter_400Regular",
+  },
+
+  inputTextFocused: {
+    borderColor: "#4EA8DE",
+  },
+
+  button: {
+    width: 52,
+    height: 52,
+    backgroundColor: "#1E6F9F",
+    borderRadius: 6,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  buttonHover: {
+    backgroundColor: "#4EA8DE",
+  },
+
+  buttonText: {
+    color: "#F2F2F2",
+    fontSize: 24,
   },
 });
